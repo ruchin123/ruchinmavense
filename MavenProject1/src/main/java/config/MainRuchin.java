e9f5b73060c6a28ccc4fd4311d66226bed3fe52a
ruchin.1987 <ruchin.1987@192.168.1.9>
2016-07-23 18:56:31 +0530

    donefor theday

---
+++ MavenProject1/src/main/java/config/MainRuchin.java
@@ -0,0 +1,37 @@
+package config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class MainRuchin {
+
+	public static void main(String[] args) {
+		
+				
+//				System.setProperty("webdriver.ie.driver","H:/SOFTWARE/SELENIUM/Driver/IEDriverServer.exe");
+//				WebDriver driver =new InternetExplorerDriver();
+				System.setProperty("webdriver.chrome.driver","H:/SOFTWARE/SELENIUM/Driver/chromedriver.exe");
+				
+				//Log.startTestCase("Selenium_Test_001");
+				
+				WebDriver driver =new ChromeDriver();
+				
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				
+				driver.get("http://newtours.demoaut.com/");
+				driver.findElement(By.xpath("//input[@name='userName']")).sendKeys("ruchin123");
+				driver.findElement(By.xpath("//input[@name='password']")).sendKeys("lamar");
+				driver.findElement(By.xpath("//input[@name='login']")).click();
+				driver.quit();
+				System.out.println("hi");
+				
+				//Log.endTestCase("Selenium_Test_001");
+
+	}
+
+}
